9d580df6af0a1180ac7fd399b89eb53c9d919db5
C-D-Lewis <Bonsitm@gmail.com>
2022-10-28 23:14:17 +0100

    Working example

--- midi-player/MIDIPlayer.py
+++ midi-player/MIDIPlayer.py
@@ -11,12 +11,13 @@ SONG_NAME = 'Still Alive'
 
 bar_progress = 0
 note_index = 0
+start_time = time.ticks_ms()
 
 #
 # Setup game
 #
 def setup():
-    thumby.display.setFPS(30)
+    thumby.display.setFPS(60)
     thumby.display.setFont("/lib/font5x7.bin", 5, 7, 1)
 
 #
@@ -25,22 +26,28 @@ def setup():
 def update():
     global bar_progress
     global note_index
+    global last_note_start
     
     total_notes = len(TRACK)
     if (note_index == total_notes):
         print('End of notes')
         time.sleep(2)
         thumby.reset()
-    
-    # Move progress bar (offset by 1 visually)
-    bar_progress = int(round(WIDTH * (note_index + 1) / total_notes))
-    
-    # Play next note
-    freq = int(PITCH_TABLE[TRACK[note_index][1]])
-    duration = int((TRACK[note_index][3] - TRACK[note_index][2]) * 1000)
-    print(TRACK[note_index], freq, duration)
-    thumby.audio.playBlocking(freq, duration)
-    note_index = note_index + 1
+        
+    # Has next note started?
+    note = TRACK[note_index]
+    if (time.ticks_ms() - start_time > note[2] * 1000):
+        # Move progress bar (offset by 1 visually)
+        bar_progress = int(round(WIDTH * (note_index + 1) / total_notes))
+        
+        # Play next note if it's time
+        freq = int(PITCH_TABLE[note[1]])
+        duration = int((note[3] - note[2]) * 1000)
+        print(note, freq, duration)
+        thumby.audio.playBlocking(freq, duration)
+        
+        # Go to next note
+        note_index = note_index + 1
     
 #
 # Main draw step
